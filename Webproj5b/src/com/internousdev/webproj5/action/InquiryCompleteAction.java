f77fb819c2a103146a5ce6158aea480d25ba02b7
Shoya1510 <bodysoul.1510.888@@gmail.com>
2019-03-01 18:23:01 +0900

    3/1

--- Webproj5b/src/com/internousdev/webproj5/action/InquiryCompleteAction.java
+++ Webproj5b/src/com/internousdev/webproj5/action/InquiryCompleteAction.java
@@ -2,18 +2,22 @@ package com.internousdev.webproj5.action;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+
+import org.apache.struts2.interceptor.SessionAware;
 
 import com.internousdev.webproj5.dao.InquiryCompleteDAO;
 import com.internousdev.webproj5.dto.InquiryDTO;
 import com.opensymphony.xwork2.ActionSupport;
 
-public class InquiryCompleteAction extends ActionSupport{
+public class InquiryCompleteAction extends ActionSupport implements SessionAware{
 
 	private String name;
 	private String qtype;
 	private String body;
 
 	List<InquiryDTO> inquiryDTOList = new ArrayList<InquiryDTO>();
+	private Map<String, Object> session;
 
 	public String execute() {
 		String ret=ERROR;
@@ -21,6 +25,7 @@ public class InquiryCompleteAction extends ActionSupport{
 		int count= dao.insert(name, qtype, body);
 		if(count > 0){
 			inquiryDTOList=dao.select();
+			session.put("inquiryDTOList", inquiryDTOList);
 
 	     ret=SUCCESS;
 		}
@@ -51,12 +56,12 @@ public class InquiryCompleteAction extends ActionSupport{
 		this.body = body;
 	}
 
-	public List<InquiryDTO> getInquiryDTOList() {
-		return inquiryDTOList;
+	public Map<String, Object> getSession() {
+		return session;
 	}
 
-	public void setInquiryDTOList(List<InquiryDTO> inquiryDTOList) {
-		this.inquiryDTOList = inquiryDTOList;
+	public void setSession(Map<String, Object> session) {
+		this.session = session;
 	}
 
 
--- Webproj5b/src/com/internousdev/webproj5/action/TestAction.java
+++ Webproj5b/src/com/internousdev/webproj5/action/TestAction.java
@@ -2,18 +2,22 @@ package com.internousdev.webproj5.action;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+
+import org.apache.struts2.interceptor.SessionAware;
 
 import com.internousdev.webproj5.dao.TestDAO;
 import com.internousdev.webproj5.dto.LoginDTO;
 import com.opensymphony.xwork2.ActionSupport;
 
-public class TestAction extends ActionSupport {
+public class TestAction extends ActionSupport implements SessionAware{
 
 	private String username;
 
 	private String password;
 
 	private List<LoginDTO> loginDTOList = new ArrayList<LoginDTO>();
+	private Map<String, Object> session;
 
 	public String execute() {
 		String ret = ERROR;
@@ -28,6 +32,7 @@ public class TestAction extends ActionSupport {
 		}
 
 		loginDTOList = dao.select(username, password);
+		session.put("loginDTOList", loginDTOList);
 
 		return ret;
 	}
@@ -48,11 +53,11 @@ public class TestAction extends ActionSupport {
 		this.password = password;
 	}
 
-	public List<LoginDTO> getLoginDTOList() {
-		return loginDTOList;
+	public Map<String, Object> getSession() {
+		return session;
 	}
 
-	public void setLoginDTOList(List<LoginDTO> loginDTOList) {
-		this.loginDTOList = loginDTOList;
+	public void setSogession(Map<String, Object> session) {
+		this.session = session;
 	}
 }
